bb13c8093949b260feaa0607da80a0df47d264a3
Tatsuhiro Ikebe <tatsuhiro.ikebe@gmail.com>
2025-05-04 20:53:46 +0900

    refactor: exec clang-format

--- include/yaml_loader.hpp
+++ include/yaml_loader.hpp
@@ -5,10 +5,11 @@
 
 extern std::string joystick_device_path;
 
-struct SecretCommand {
-    std::string name;
-    std::vector<std::string> combo;
-    std::string action;
+struct SecretCommand
+{
+  std::string name;
+  std::vector<std::string> combo;
+  std::string action;
 };
 
-std::vector<SecretCommand> load_commands(const std::string& filepath);
+std::vector<SecretCommand> load_commands(const std::string & filepath);
